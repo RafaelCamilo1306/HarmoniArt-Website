c1f0018462fb912c6c4fd1c814e143b5944ea88f
Rafael Camilo Pradal <rafael.camilo1306@gmail.com>
2022-09-01 16:56:13 -0300

    commit organização de arquivos
    deletei arquivos não utilizados, acrescentei futuras pagina, nas partes do sistema, bem como separei os códigos de JS do HTML, colocando em arquivos separados e os linkando-os

---
+++ HarmoniArt_web/JavaScript/carrossel.js
@@ -0,0 +1,27 @@
+function actualizeLabelColor(labelId) {
+    for (let i = 1; i <= 5; i++) {
+        let label = document.getElementById("label" + i);
+        label.style.background = "transparent";
+        label.style.borderColor = "#FFF";
+
+    }
+    let selectedLabel = document.getElementById("label" + labelId);
+    selectedLabel.style.background = "#FF9D02";
+    selectedLabel.style.borderColor = "#FF9D02";
+
+    let s1=document.getElementById("s1")
+    if (labelId == "1"){
+        s1.style.marginLeft="0";
+    }else if (labelId == "2"){
+        s1.style.marginLeft="-20%";
+    }else if (labelId == "3"){
+        s1.style.marginLeft="-40%";
+    }else if (labelId == "4"){
+        s1.style.marginLeft="-60%";
+    }else{
+        s1.style.marginLeft="-80%";
+    }
+}
+const defaultLabel = document.getElementById("label1");
+defaultLabel.style.background = "#FF9D02";
+defaultLabel.style.borderColor = "#FF9D02";
