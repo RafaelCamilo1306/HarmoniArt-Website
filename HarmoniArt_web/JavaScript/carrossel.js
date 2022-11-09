27b795325383a015a87d525bfb15171fa3ec8171
Rafael Camilo Pradal <rafael.camilo1306@gmail.com>
2022-11-09 08:56:57 -0300

    update responsivity
    melhorei a responsividade do carrossel e da área dos cursos oferecidos pelo cliente de forma que a versão mobile ficasse adequada

--- HarmoniArt_web/JavaScript/carrossel.js
+++ HarmoniArt_web/JavaScript/carrossel.js
@@ -9,17 +9,17 @@ function actualizeLabelColor(labelId) {
     selectedLabel.style.background = "#FF9D02";
     selectedLabel.style.borderColor = "#FF9D02";
 
-    let s1=document.getElementById("s1")
-    if (labelId == "1"){
-        s1.style.marginLeft="0";
-    }else if (labelId == "2"){
-        s1.style.marginLeft="-20%";
-    }else if (labelId == "3"){
-        s1.style.marginLeft="-40%";
-    }else if (labelId == "4"){
-        s1.style.marginLeft="-60%";
-    }else{
-        s1.style.marginLeft="-80%";
+    let s1 = document.getElementById("s1")
+    if (labelId == "1") {
+        s1.style.marginLeft = "0";
+    } else if (labelId == "2") {
+        s1.style.marginLeft = "-20%";
+    } else if (labelId == "3") {
+        s1.style.marginLeft = "-40%";
+    } else if (labelId == "4") {
+        s1.style.marginLeft = "-60%";
+    } else {
+        s1.style.marginLeft = "-80%";
     }
 }
 const defaultLabel = document.getElementById("label1");
@@ -29,10 +29,10 @@ defaultLabel.style.borderColor = "#FF9D02";
 
 /*active-botão do menu burguer*/
 
-const burguer= document.querySelector(".mobile-menu");
+const burguer = document.querySelector(".mobile-menu");
 const navmenu = document.querySelector(".sections");
 
-burguer.addEventListener("click", () =>{
+burguer.addEventListener("click", () => {
 
     burguer.classList.toggle('active')
     navmenu.classList.toggle('active')
@@ -41,23 +41,23 @@ burguer.addEventListener("click", () =>{
 // Function para a criação do pop-up
 function iniciaModal(modalID) {
     const modal = document.getElementById(modalID);
-    if (modal==document.getElementById(modalID)) {
+    if (modal == document.getElementById(modalID)) {
         // condição parar verificar se na varivel contem algum elemento
         modal.classList.add('mostrar');
         modal.addEventListener('click', (e) => {
-            if (e.target.id == modalID || e.target.className== 'fechar') {
+            if (e.target.id == modalID || e.target.className == 'fechar') {
                 // condição para verificar se o usuário clicou o no espaço em volta do modal ou no botão de fechar caso retorne 'true' o modal fechará
                 modal.classList.remove('mostrar');
             }
         });
-    }else{
+    } else {
         alert("erro interno, para contato acesse nossas redes sociais ou nosso whats");
     }
 }
-    
+
 // variavel que armazena o botão 
 const subscribe = document.querySelector('#bntSubs');
-    
+
 // evento de click para ativar a função;
 subscribe.addEventListener('click', () => iniciaModal('modalSubscribe'));
 
@@ -65,19 +65,21 @@ subscribe.addEventListener('click', () => iniciaModal('modalSubscribe'));
 
 // botão back to top ------------------------
 
-const botao= document.querySelector(".bnt-backTop");
-
+const botao = document.querySelector(".bnt-backTop");
+const ico = document.querySelector("#span-btnTop")
 window.addEventListener("scroll", () => {
-    if(window.scrollY==0){
+    if (window.scrollY == 0) {
         botao.classList.remove("active");
-    }else if (window.scrollY < 4700) {
-        botao.style.background="var(--corPrimariaMedium)";
+    } else if (window.scrollY < 4700) {
+        botao.style.background = "var(--corPrimariaMedium)";
+        ico.style.color = "var(--corPrimariaDark100)"
         botao.classList.add("active");
-            botao.addEventListener("click", ()=>{
-                window.scrollTo(0, 0);
-            })
-     }else {
-        botao.style.background="var(--corGray300)";
+        botao.addEventListener("click", () => {
+            window.scrollTo(0, 0);
+        })
+    } else {
+        botao.style.background = "var( --corPrimariaDark100)";
+        ico.style.color = "var(--corPrimariaMedium)";
     }
 });
 
