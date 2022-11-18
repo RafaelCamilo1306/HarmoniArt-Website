9bd75783c01863755f2d4537aba630d23aaa97a3
Rafael Camilo Pradal <rafael.camilo1306@gmail.com>
2022-11-17 22:06:11 -0300

    Update correção de incoerências
    Resolvido algumas incoerências no projetos, remoção de algumas informações extras, não utilizadas e acréscimo de outras não implementadas antes

--- HarmoniArt_web/JavaScript/statusCad.js
+++ HarmoniArt_web/JavaScript/statusCad.js
@@ -4,13 +4,13 @@ function statusOnload() {
     let statusMt = document.querySelector('#statusMt');
     let matricula = document.querySelector('#botaoMt');
     if (payment.checked) {
-        statusPg.textContent = "Pendente";
-        statusPg.style.color = "var(--corAuxiliarDangerous)"
+        statusPg.textContent = "Pago";
+        statusPg.style.color = "var(--corAuxiliarSucess)"
     }
 
     if (matricula.checked) {
-        statusMt.textContent = "Pendente";
-        statusMt.style.color = "var(--corAuxiliarDangerous)"
+        statusMt.textContent = "Ativo";
+        statusMt.style.color = "var(--corAuxiliarSucess)"
     }
 
 }
@@ -18,26 +18,25 @@ function statusOnload() {
 function pagSituation() {
 
     let status = document.querySelector('#statusPg');
-    console.log(status.textContent);
-    if (status.textContent === "Pago") {
-        status.textContent = "Pendente";
-        status.style.color = "var(--corAuxiliarDangerous)"
-
-    } else {
+    if (status.textContent === "Pendente") {
         status.textContent = "Pago";
         status.style.color = "var(--corAuxiliarSucess)"
-
+        
+    } else {
+        status.textContent = "Pendente";
+        status.style.color = "var(--corAuxiliarDangerous)"
+        
     }
 }
 function matSituation() {
 
     let status = document.querySelector('#statusMt');
     console.log(status.textContent);
-    if (status.textContent === "Ativo") {
-        status.textContent = "Inativo";
-        status.style.color = "var(--corAuxiliarDangerous)"
-    } else {
+    if (status.textContent === "Inativo") {
         status.textContent = "Ativo";
         status.style.color = "var(--corAuxiliarSucess)"
+    } else {
+        status.textContent = "Inativo";
+        status.style.color = "var(--corAuxiliarDangerous)"
     }
 }
