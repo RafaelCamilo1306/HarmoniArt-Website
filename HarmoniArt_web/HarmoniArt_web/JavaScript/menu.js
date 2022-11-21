97a6a4ddf5af214a1a6d9d5ddf54f9f84472ec5b
Rafael Camilo Pradal <rafael.camilo1306@gmail.com>
2022-11-21 20:04:43 -0300

    deletei a pasta do back-end

--- HarmoniArt_web/HarmoniArt_web/JavaScript/menu.js
+++
@@ -1,8 +0,0 @@
-const burguer = document.querySelector(".mobile-menu");
-const navmenu = document.querySelector(".sections");
-
-burguer.addEventListener("click", () => {
-
-    burguer.classList.toggle('active')
-    navmenu.classList.toggle('active')
-})
