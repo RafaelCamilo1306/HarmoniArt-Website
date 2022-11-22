d522fb176410a88d1312ca47d36240f3538f2437
Rafael Camilo Pradal <rafael.camilo1306@gmail.com>
2022-11-21 22:59:02 -0300

    Commit lembrete e agenda created
    criei os lembreste e tbm a area para criar um lembrete, e comecei a agenda de aulas

--- HarmoniArt_web/JavaScript/growFrame.js
+++ HarmoniArt_web/JavaScript/growFrame.js
@@ -1,14 +1,17 @@
 function growIframe(link){
     const perfil = document.querySelector(".divPerfil");
     const frame= document.querySelector(".home-content");
-    if (link=="dados" || link=="agenda"){
+    if (link=="dados"){
         if(window.innerWidth<768){
             perfil.style.display='none';
             frame.style.border='none';
         }else{
             perfil.style.display='flex';
         }
-    }else{
+    }else if(link=="agenda"){
+            perfil.style.display='none';
+            frame.style.width='100%';
+        }else{
         perfil.style.display='flex';
     }
 }
