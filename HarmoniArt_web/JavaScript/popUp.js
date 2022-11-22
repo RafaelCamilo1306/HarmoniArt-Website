38d313861bf47eda46c161b308058be52981e37c
Rafael Camilo Pradal <rafael.camilo1306@gmail.com>
2022-11-22 19:39:17 -0300

    commit agenda grade
    criei a grade da agenda versão mobile e desktop

--- HarmoniArt_web/JavaScript/popUp.js
+++ HarmoniArt_web/JavaScript/popUp.js
@@ -1,14 +1,15 @@
-let subscribe = document.querySelector('#bntSubs');
+let bnt = document.querySelector('#bntPop');
+let exl = document.querySelector(".del")
 // evento de click para ativar a função;
-subscribe.addEventListener('click', () => iniciaModal('modalSubscribe'));
+bnt.addEventListener('click', () => iniciaModal('modal'));
 
 function iniciaModal(modalID) {
     const modal = document.getElementById(modalID);
-    if (modal == document.getElementById(modalID)) {
+    if (modal.id == 'modal') {
         // condição parar verificar se na varivel contem algum elemento
         modal.classList.add('mostrar');
         modal.addEventListener('click', (e) => {
-            if (e.target.id == modalID || e.target.className == 'fechar') {
+            if (e.target.id == modalID || e.target.className == 'fechar'|| e.target.id=='cancel' ) {
                 // condição para verificar se o usuário clicou o no espaço em volta do modal ou no botão de fechar caso retorne 'true' o modal fechará
                 modal.classList.remove('mostrar');
             }
