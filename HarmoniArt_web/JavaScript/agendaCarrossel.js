33fee6de0d24ac2f8bb911dd3265d54279d8e770
Rafael Camilo Pradal <rafael.camilo1306@gmail.com>
2022-11-23 18:13:54 -0300

    commit carrossel agenda ="acabei"
    terminei o carrossel da agenda

--- HarmoniArt_web/JavaScript/agendaCarrossel.js
+++ HarmoniArt_web/JavaScript/agendaCarrossel.js
@@ -2,7 +2,13 @@ var margin= document.querySelector(".grade");
 let next= document.getElementById("avancar");
 let back = document.getElementById("voltar");
 var cont=1;
-    
+function zeraCont(){
+    if(cont<=0){
+        cont=1;
+    }else if(cont>6){
+        cont=6;
+    }
+}
 function carrossel(){
 
     if(margin.style.marginLeft==""){
@@ -13,12 +19,25 @@ function carrossel(){
 }
 
 next.addEventListener('click', () => {
-    console.log(cont);
     if(cont==6){
         margin.style.marginLeft = '-'+[cont]+'00%'; 
         next.style.opacity=0;
     }else{
         back.style.opacity=1;
+        margin.style.marginLeft = '-'+[cont]+'00%'; 
+    }
+    cont++
+    console.log(cont);  
+});
+
+back.addEventListener('click', () => {
+    console.log(cont);
+    cont=cont-2;
+    if(cont<=1){
+            back.style.opacity=0;
+            margin.style.marginLeft = '-'+[cont]+'00%'; 
+    }else{
+        next.style.opacity=1;
         margin.style.marginLeft = '-'+[cont]+'00%'; 
     }
     cont++
