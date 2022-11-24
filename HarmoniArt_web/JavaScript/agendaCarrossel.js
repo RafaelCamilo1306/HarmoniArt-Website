f6fe4f76aa8697ba27bab39fd5667485ac840a76
Rafael Camilo Pradal <rafael.camilo1306@gmail.com>
2022-11-24 10:18:46 -0300

    commit carrossel="end";
    terminei (pedi ajuda) o carrossel da agenda

--- HarmoniArt_web/JavaScript/agendaCarrossel.js
+++ HarmoniArt_web/JavaScript/agendaCarrossel.js
@@ -1,16 +1,16 @@
 var margin= document.querySelector(".grade");
 let next= document.getElementById("avancar");
 let back = document.getElementById("voltar");
-var cont=1;
-function zeraCont(){
-    if(cont<=0){
-        cont=1;
-    }else if(cont>6){
-        cont=6;
-    }
-}
-function carrossel(){
+var cont = 0;
 
+const disabledButtons = {
+    back: false,
+    next: false
+}
+
+actualizeOpacity(0);
+
+function carrossel(){
     if(margin.style.marginLeft==""){
         back.style.opacity=0;
     }else{
@@ -18,27 +18,36 @@ function carrossel(){
     }
 }
 
-next.addEventListener('click', () => {
-    if(cont==6){
-        margin.style.marginLeft = '-'+[cont]+'00%'; 
-        next.style.opacity=0;
-    }else{
-        back.style.opacity=1;
-        margin.style.marginLeft = '-'+[cont]+'00%'; 
+function actualizeOpacity(nextValue) {
+    disabledButtons.back = false;
+    disabledButtons.next = false;
+    back.style.opacity = 1;
+    next.style.opacity = 1;
+
+    if(nextValue == 6) {
+        next.style.opacity = 0;
+        disabledButtons.next = true;
     }
-    cont++
-    console.log(cont);  
+    if(nextValue == 0) {
+        back.style.opacity = 0;
+        disabledButtons.back = true;
+    }
+}
+
+function changePage(page) {
+    if(page < 0) page = 7 + page;
+    margin.style.marginLeft = '-'+(page % 7)+'00%';
+    return page % 7;
+}
+
+next.addEventListener('click', () => {
+    if(disabledButtons.next) return;
+    actualizeOpacity(cont + 1);
+    cont = changePage(cont + 1);
 });
 
 back.addEventListener('click', () => {
-    console.log(cont);
-    cont=cont-2;
-    if(cont<=1){
-            back.style.opacity=0;
-            margin.style.marginLeft = '-'+[cont]+'00%'; 
-    }else{
-        next.style.opacity=1;
-        margin.style.marginLeft = '-'+[cont]+'00%'; 
-    }
-    cont++
+    if(disabledButtons.back) return;
+    actualizeOpacity(cont - 1);
+    cont = changePage(cont - 1);
 })
